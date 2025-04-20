8b79435b089d31c17a306c3403ed269d75192c3d
Anwarjondev <anvarjonturdaliyev66@gmail.com>
2025-04-20 16:21:30 +0500

    change env

--- handlers/auth_handlers.go
+++ handlers/auth_handlers.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -17,10 +19,11 @@ import (
 var jwtkey []byte
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("Warning: .env file not found, falling back to OS env variables")
 	}
+
+	
 	jwtkey = []byte(os.Getenv("JWT_KEY"))
 }
 
