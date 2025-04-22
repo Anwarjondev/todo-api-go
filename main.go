421e389ccb0516d0908a8c66a91d4b950e892cf3
Anwarjondev <anvarjonturdaliyev66@gmail.com>
2025-04-22 20:19:11 +0500

    add a function for fix CORS problem

--- main.go
+++ main.go
@@ -24,11 +24,13 @@ import (
 // @contact.email support@example.com
 // @license.name MIT
 // @license.url https://opensource.org/licenses/MIT
-// @host localhost:8080
+// @host todo-api-go-production-0484.up.railway.app
 // @BasePath /
+// @schemes https
 // @securityDefinitions.apikey BearerAuth
 // @in header
 // @name Authorization
+// @description JWT Authorization header using the Bearer scheme. Example: "Bearer {token}"
 func main() {
 
 	db.InitDB()
@@ -37,5 +39,19 @@ func main() {
 	routes.SetupRoutes(mux)
 	mux.Handle("/swagger/", httpSwagger.WrapHandler)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", enableCORS(mux))
+}
+func enableCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
